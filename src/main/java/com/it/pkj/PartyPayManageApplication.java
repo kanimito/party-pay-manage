af2ce27ffca188f91b467e14a644d4594b63a2f0
可可建 <3189901836@qq.com>
2025-03-22 20:42:11 +0800

    添加mysql，mybatisplus

--- src/main/java/com/it/pkj/PartyPayManageApplication.java
+++ src/main/java/com/it/pkj/PartyPayManageApplication.java
@@ -1,9 +1,11 @@
 package com.it.pkj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.it.pkj.mapper")
 public class PartyPayManageApplication {
 
     public static void main(String[] args) {
