677dd966c8169c29cea135e7013c7e5fdec225e1
可可建 <3189901836@qq.com>
2025-03-22 20:30:19 +0800

    创建项目

---
+++ src/main/java/com/it/pkj/PartyPayManageApplication.java
@@ -0,0 +1,13 @@
+package com.it.pkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyPayManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyPayManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/it/pkj/PartyPayManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.pkj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyPayManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
